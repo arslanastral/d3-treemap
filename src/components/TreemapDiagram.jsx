cfec2adf9ab321ac0ebae1f2501d8d8a46743304
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-02-09 20:34:02 +0800

    removed comments

--- src/components/TreemapDiagram.jsx
+++ src/components/TreemapDiagram.jsx
@@ -58,15 +58,11 @@ const TreemapDiagram = () => {
   ]);
   const wrapperRef = useRef();
   const dimensions = useResizeObserver(wrapperRef);
-  // const legendContainerRef = useRef();
-  // const legend = useRef();
-  // const legendDimensions = useResizeObserver(legendContainerRef);
   const dataURL =
     "https://gist.githubusercontent.com/arslanastral/3570f49c8ffa0731cbafc8560532927c/raw/1372a51076f733638604105a39fd2c158daddaa4/top-grossing-media-franchises.csv";
 
   useEffect(() => {
     const svg = d3.select(TreemapDiagramRef.current);
-    // const LegendSvg = d3.select(legend.current);
     const { width, height } =
       dimensions || wrapperRef.current.getBoundingClientRect();
 
@@ -122,123 +118,6 @@ const TreemapDiagram = () => {
       "#656d61",
       "#436f90",
       "#5e7304",
-      "#82b792",
-      "#fb88a3",
-      "#dd8df2",
-      "#6a5cc0",
-      "#d098d5",
-      "#ac15dc",
-      "#a4543b",
-      "#76b4cc",
-      "#6963a4",
-      "#8e620d",
-      "#77adf8",
-      "#c9a918",
-      "#99537d",
-      "#acaf7d",
-      "#7850d5",
-      "#ae3a9f",
-      "#68bd74",
-      "#e09d60",
-      "#1069cd",
-      "#d50438",
-      "#c03d17",
-      "#79b6af",
-      "#517430",
-      "#db9b94",
-      "#095cf8",
-      "#b1b045",
-      "#c0a4a9",
-      "#bc01c1",
-      "#906033",
-      "#e49c3f",
-      "#8e4db9",
-      "#bb3a64",
-      "#cb1478",
-      "#776b09",
-      "#94b743",
-      "#763eff",
-      "#1a7a3e",
-      "#617046",
-      "#915c62",
-      "#ec8dc0",
-      "#ba22ac",
-      "#437461",
-      "#913ddc",
-      "#4bbda8",
-      "#b4482e",
-      "#a9a5e3",
-      "#78b1e2",
-      "#855b91",
-      "#fc902e",
-      "#2cbada",
-      "#64c104",
-      "#8abb0b",
-      "#3cc441",
-      "#68be5c",
-      "#b9ac66",
-      "#11c37b",
-      "#5e6c7c",
-      "#686690",
-      "#f09469",
-      "#66bc8a",
-      "#736a4e",
-      "#776768",
-      "#c43251",
-      "#c1a0c6",
-      "#a2acb7",
-      "#457713",
-      "#f87fe4",
-      "#c1a693",
-      "#b14949",
-      "#487175",
-      "#eb929c",
-      "#e18fdc",
-      "#326ea4",
-      "#147861",
-      "#9b584f",
-      "#dba103",
-      "#cca567",
-      "#5464b9",
-      "#c797f2",
-      "#94b57c",
-      "#d3084b",
-      "#e09b7e",
-      "#cd2729",
-      "#525ae2",
-      "#a04c8a",
-      "#bb308b",
-      "#1d7489",
-      "#a82bce",
-      "#ee9751",
-      "#a94b70",
-      "#9432ea",
-      "#9c5a24",
-      "#9cb193",
-      "#816722",
-      "#826540",
-      "#fb8b8e",
-      "#696f20",
-      "#33b4ff",
-      "#d3a434",
-      "#7b5aab",
-      "#5b5bd4",
-      "#c22c71",
-      "#ca2f01",
-      "#34792f",
-      "#81bb4c",
-      "#3064d4",
-      "#80ba6d",
-      "#4f68ab",
-      "#b6a5bf",
-      "#8a5d76",
-      "#dc9f50",
-      "#935e41",
-      "#a94491",
-      "#147953",
-      "#8cb1be",
-      "#41c183",
-      "#acb05e",
     ];
 
     let color = d3
@@ -264,8 +143,6 @@ const TreemapDiagram = () => {
       .duration(1000)
       .attr("x", (d) => d.x0)
       .attr("y", (d) => d.y0)
-      // .attr("rx", "3px")
-      // .attr("ry", "3px")
       .attr("width", (d) => d.x1 - d.x0)
       .attr("height", (d) => d.y1 - d.y0)
       .attr("fill", (d) => color(d.data[currentDataType]));
@@ -276,7 +153,6 @@ const TreemapDiagram = () => {
       mediaTypeTitle = svg
         .selectAll("text")
         .data(root.children)
-
         .join("text")
         .attr("class", "block-type-title")
         .text(function (d) {
